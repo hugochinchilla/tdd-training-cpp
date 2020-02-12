a9a827b63978518f645bdcf5d029c82be28c6b5b
Jordi Anguela <jordi@codium.team>
2020-02-12 10:57:29 +0100

    print-date: add PrintDate logic

--- print-date/Main.cpp
+++ print-date/Main.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
+#include "PrintDate.h"
 
 using namespace std;
 
 int main(int, char *[]) {
-    cout << "-- PrintDate kata --" << endl;
-    cout << "------- END --------" << endl;
+    cout << "---- PrintDate kata ----" << endl;
+
+    PrintDate printDate;
+    printDate.printCurrentDate();
+
+    cout << "--------- END ----------" << endl;
     return 0;
 }
--- print-date/PrintDate.h
+++ print-date/PrintDate.h
@@ -1,4 +1,11 @@
 #ifndef TDD_TRAINING_CPP_PRINTDATE_H
 #define TDD_TRAINING_CPP_PRINTDATE_H
 
+
+class PrintDate {
+public:
+    PrintDate();
+    void printCurrentDate();
+};
+
 #endif //TDD_TRAINING_CPP_PRINTDATE_H
