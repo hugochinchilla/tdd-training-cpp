a4d6e6aa4256ffd0eb040e63a9a5195028c985a7
Luis Rovirosa <luisrovirosa@gmail.com>
2020-02-12 05:37:03 +0100

    add PasswordValidator skeleton

---
+++ password-validator/PasswordValidatorTest.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include "PasswordValidator.h"
+
+TEST(PasswordValidatorTest, TheTestName) {
+
+    EXPECT_EQ("foo", "foo");
+}
+
---
+++ password-validator/run-once.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+make
+./PasswordValidator
+make clean
